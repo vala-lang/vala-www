3cd64892b27eb6b041211f43dd730f4d7328798d
Nahuel Gomez Castro <nahual_gomca@outlook.com.ar>
2021-05-06 09:15:22 -0300

    next.config.js: Added to prefix public resources

---
+++ next.config.js
@@ -0,0 +1,7 @@
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  assetPrefix: isProd
+    ? 'https://cdn.statically.io/gh/nahuelwexd/vala-website/gh-pages/'
+    : '',
+}
