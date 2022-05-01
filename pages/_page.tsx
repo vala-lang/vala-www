b0dd9a975aac1df56dd57b66b73e964906170769
colinkiama <colinkiama@gmail.com>
2022-05-01 02:56:51 +0100

    Update Header links naming and ordering

--- pages/_page.tsx
+++ pages/_page.tsx
@@ -12,14 +12,12 @@ const Page: FC = (props) => (
         <Header.NavLink href="/about">About</Header.NavLink>
         <Header.NavLink href="/learn">Learn</Header.NavLink>
         <Header.NavLink href="/community">Community</Header.NavLink>
+        <Header.NavLink href="/blog">Blog</Header.NavLink>
         <Header.NavLink href="https://valadoc.org" external>
           API Reference
         </Header.NavLink>
         <Header.NavLink href="https://gitlab.gnome.org/GNOME/vala" external>
           Source Code
-        </Header.NavLink>
-        <Header.NavLink href="https://planet.vala-project.org" external>
-          News
         </Header.NavLink>
       </Header>
       {props.children}
