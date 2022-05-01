4976143d1b5fa1e15117959ea67180b8ee4cc79b
colinkiama <colinkiama@gmail.com>
2022-05-01 01:25:23 +0100

    Add plans for smarter build configurations

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 const isProd = process.env.NODE_ENV === 'production'
 
+// TODO: Also utilise environment variables to figure out
+// whether this is a local build, GitHub pages build or a public build
 module.exports = {
   assetPrefix: isProd
     ? 'https://cdn.statically.io/gh/nahuelwexd/vala-website/gh-pages/'
