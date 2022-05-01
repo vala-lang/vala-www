85db2a7ac50ff7ffa9ea75ea7a1904f456eb15c4
colinkiama <colinkiama@gmail.com>
2022-05-01 02:57:15 +0100

    Fix next js config

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const isProd = process.env.NODE_ENV === 'production'
 // whether this is a local build, GitHub pages build or a public build
 module.exports = {
   assetPrefix: isProd
-    ? 'https://cdn.statically.io/gh/nahuelwexd/vala-website/gh-pages/'
+    ? 'https://cdn.statically.io/gh/colinkiama/vala-www/gh-pages/'
     : '',
   basePath: isProd ? '/vala-www' : '',
 }
