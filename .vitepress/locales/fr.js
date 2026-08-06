a9d8e656db0422efbbe2e2efc6b03bd856419372
Ifrah Gul <ifrahgul618@gmail.com>
2026-08-06 02:52:46 -0700

    Translate remaining untranslated strings in French locale (#251)
    * Translate remaining untranslated strings in French locale
    
    * Fix indentation in French locale

--- .vitepress/locales/fr.js
+++ .vitepress/locales/fr.js
@@ -86,15 +86,16 @@ export default {
 
   page_not_found: 'Erreur 404 - Page non trouvé',
   page_not_found_description: "la page que vous avez demandée n'existe plus.",
-  go_home: 'Go Home',
 
-  current_versions: 'Current Versions',
-  install_how: 'How to install',
-  stable: 'Stable',
-  stable_lts: 'Stable Long-term Support',
-  devel: 'Development',
+  go_home: "Retour à l'accueil",
 
-  roadmap: 'Roadmap',
+  current_versions: "Versions actuelles",
+  install_how: "Comment installer",
+  stable: "Stable",
+  stable_lts: "Stable à long terme",
+  devel: "Développement",
+
+  roadmap: "Feuille de route",
 
   site_title: 'Langage de programmation Vala',
   site_description:
